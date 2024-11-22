a2c94b0873f97c1b777d0105ba469fa089432271
Ugolovka <medusa_gangrena@tutamail.com>
2024-11-22 12:58:01 +0300

    final

--- search.py
+++ search.py
@@ -1,20 +1,43 @@
+import random
 from random import randint as ri
+list_1 = random.sample(range(1000001), 100)#sample позволяет выбрать случайные элементы из указанного диапазона без дубликатов.
+print(list_1)
 
-list = []
-for i in range(0,100):
-    list.append(ri(0, 100_000))
-aim  = 4567
+print('ВТОРОЙ СПИСОК')
+list_2 = random.sample(range(1000001), 100)#sample позволяет выбрать случайные элементы из указанного диапазона без дубликатов.
+list_2.sort()
+print(list_2)
 
-first = 0
-last = len(list)-1
-index = -1
-while (first <= last) and (index == -1):
-    mid = (first+last)//2
-    if list[mid] == aim:
-        index = mid
-    else:
-        if aim<list[mid]:
-            last = mid -1
-        else:
-            first = mid +1
-print(index)
+el = int(input('искомый элемент:'))
+
+count = 0 #начинаем подсчет с 0 количество сравнений
+if el in list_1: #если эле-т находится в list_1
+    for i in range(0, len(list_1)):
+        count = count + 1 #прибавляем каждый цикл +1
+        if list_1[i] == el:
+            break 
+    print(f'Индекс искомого эл-та из первого списка:{i}')
+    print(f'Количество сравнений в линейном поиске:{count}')
+else:
+    print("Элемент не найден в первом списке")
+
+#бинарный поиск
+if el in list_2: #проверка на наличие в списке
+    first = 0
+    last = len(list_2)-1
+    count2 = 0
+    index = 1
+    while (first <= last) and index == 1:
+        mid = (first+last)//2 # индекс элемента в середине списка
+        count2 = count2 + 1 #прибавляем каждый цикл +1
+        if list_2[mid] == el:
+            index = mid
+        else: 
+            if el<list_2[mid]:
+                last = mid -1
+            else:
+                first = mid +1
+    print(f'Количество сравнений в бинарном поиске:{count2}')
+    print(f'Индекс искомого эл-та из второго списка:{index}')
+else:
+    print("Элемент не найден во втором списке")
--- sort.py
+++ sort.py
@@ -1,17 +1,32 @@
-from random import randint as ri
-
-list = []
-for i in range(0,100):
-    list.append(ri(0, 100_000))
-unsorted_list = list
-sorted_list = []
-
-while len(unsorted_list) > 0:
-    min = unsorted_list[0]
-    for i in range(0, len(unsorted_list)):
-        if unsorted_list[i] > min:
-            min = unsorted_list[i]
-    sorted_list.append(min)
-    unsorted_list.remove(min)
-
-print(sorted_list)
+import random as ran
+#сортировка пузырьком
+list_1 = [ran.randint(0, 1000000) for t in range(100)] #случайные элементы из указанного диапазона
+count1 = 0 #начинаем подсчет с 0 количество сравнений
+stop_flag = True # стоп флаг если он имеет значение true запускает цикл
+while stop_flag:
+    stop_flag = False # меняем значение true, если программа не сделает изменене она завершиться 
+    print(list_1) #промежуточный
+    for i in range(len(list_1) - 1): # -1 потому что в дальнейшем мы сравниваем с i + 1 
+        count1 += 1 #прибавляем каждый цикл +1
+        if list_1[i] < list_1[i + 1]: 
+            a = list_1[i] 
+            list_1[i] = list_1[i + 1] #меняем местами
+            list_1[i + 1] = a #меняем местами
+            stop_flag = True
+#сортировка выбором
+list_2 = [ran.randint(0, 1000000) for u in range(100)]
+count2 = 0
+for i in range(0 ,len(list_2)):
+    min = i
+    for j in range(i + 1,len(list_2)):
+        count2 += 1
+        print(list_2) #промежуточный
+        if list_2[j] > list_2[min]:
+            min = j
+    b = list_2[i]
+    list_2[i] = list_2[min]
+    list_2[min] = b
+print(f"Первый отсортированный список:{list_1}")
+print(f"Второй отсортированный список:{list_2}") 
+print(f'количество сравнений в первом списке:{count1}')
+print(f'количество сравнений во втором списке:{count2}')
